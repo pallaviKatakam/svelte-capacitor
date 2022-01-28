7730470a1312050ad185004b626ab62f8078925c
pallaviKatakam <pallavikatakam99@gmail.com>
2022-01-28 12:12:37 +0530

    capacitor apis

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script lang="typescript">
+<script lang="ts">
 	import { Router } from "@roxi/routify";
 	import { routes } from "../.routify/routes";
 </script>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,17 +1,142 @@
 <script>
+    // @ts-ignore
+    import { Geolocation } from '@capacitor/geolocation';
+    import { BleClient } from '@capacitor-community/bluetooth-le';
+    import { Device } from '@capacitor/device';
+    import { Keyboard } from '@capacitor/keyboard';
+    import { StatusBar } from '@capacitor/status-bar';
+    import { Camera, CameraResultType } from '@capacitor/camera';
+
+    let info = new Object;
+    let battery_info = {};
+    let src = "";
+
+    async function getDeviceInfo(){
+        info = await Device.getInfo();
+    };
+
+    getDeviceInfo();
+
+    async function getBatteryInfo(){
+        battery_info = await Device.getBatteryInfo();
+    };
+
+    getBatteryInfo();
+
+    let Bstatus = null;
+    BleClient.initialize();
+    async function getBluetoothStatus(){
+        const res = await BleClient.isEnabled();
+        Bstatus = res;
+    }
+
+    async function connectBluetooth(){
+        const res1 = await BleClient.requestDevice();
+        const res = await BleClient.connect("[9C:19:C2:22:35:18] Redmi Earbud 3 Pro");
+    }
+
+    async function onBluetooth(){
+        await BleClient.enable();
+    }
+
+    async function offBluetooth(){
+        await BleClient.disable();
+    }
+
+    async function showStatus(){
+        await StatusBar.show();
+        await StatusBar.setBackgroundColor({ color: '#FF0000' });
+    }
+    
+
+    let loc = null;
+    async function getCurrentPosition(){
+        const res = await Geolocation.getCurrentPosition()
+        loc = res
+    }
+
+    const takePicture = async () => {
+
+        const image = await Camera.getPhoto({
+            quality: 90,
+            allowEditing: true,
+            resultType: CameraResultType.Uri
+        });
+
+        src = image.path;
+    };
+
     var name = "svelte"
     var image = "svelte_cap.png"
 </script>
 
 <div class="main">
-    <img src={image}/>
+    <img alt="svelte_img" src={image}/>
     <h1>Welcome to {name}!</h1>
+
+    {JSON.stringify(info)}
+    <br>
+    <p>Device Name : {info['name']}</p>
+
+    {JSON.stringify(battery_info)}
+
+    <h1>Geolocation</h1>
+    <p>Your location is:</p>
+    <p>Latitude: {loc?.coords.latitude}</p>
+    <p>Longitude: {loc?.coords.longitude}</p>
+  
+    <button on:click={getCurrentPosition}>
+      Get Current Location
+    </button>
+
+    <p>Bluetooth status: {Bstatus}</p>
+    <button on:click={getBluetoothStatus}>
+        Get Bluetooth Status
+    </button>
+
+    <button on:click={connectBluetooth}>
+        Connect Bluetooth
+    </button>
+
+    <button on:click={onBluetooth}>
+        On Blutooth
+    </button>
+
+    <button on:click={offBluetooth}>
+        Off Blutooth
+    </button>
+
+    <button on:click={()=>Keyboard.show()}>
+        Show Keyboard
+    </button>
+
+    <button on:click={()=>Keyboard.hide()}>
+        Hide Keyboard
+    </button>
+
+    <button on:click={showStatus}>
+        Show Status
+    </button>
+
+    <button on:click={()=>StatusBar.hide()}>
+        Hide Status
+    </button>
+
+    <button on:click={takePicture}>
+        Take picture
+    </button>
+
+    <div>
+        <img {src} alt="cam_img" id="camImage"/>
+    </div>
 </div>
 
 <style>
+
     .main {
         text-align: center;
         margin: 50px auto;
+        word-break: break-all;
     }
 
     img {
